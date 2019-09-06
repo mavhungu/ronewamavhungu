19223c34a6346b8abe91bcf9f9c524c96631b370
mavhungu <ronewamavhungu@gmail.com>
2019-09-06 11:04:56 +0200

    starting afresh

---
+++ js/scrol.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    
+    lightbox.option({
+        'wrapAround': true
+      })
+
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+
+        if(position >=350){
+            $('.gallery').addClass('change');
+        }else{
+            $('.gallery').removeClass('change'); 
+        }
+
+    });
+    
+    $('.hamburger-menu').click(function(){
+        $('.navigation').toggleClass('change');
+    });
+
+});
