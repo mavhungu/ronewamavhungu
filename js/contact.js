05a28ee670a75dd51b5e14065c248b62c6aa5841
mavhungu <ronewamavhungu@gmail.com>
2019-09-15 16:56:44 +0200

    hide error

--- js/contact.js
+++ js/contact.js
@@ -12,18 +12,22 @@ if(fullname ===""){
 if(email ===""){
     document.getElementById('e-mail').innerHTML =("Email cannot be empty");
     $('.e-mail').addClass('status');
+    $('.fullnames').hide();
     return false;
 } else {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test(email)){
         document.getElementById('e-mail').innerHTML = ("Email format invalid");
         $('.e-mail').addClass('status');
+        $('.fullnames').hide();
         return false;
     }
 }
 if(message ===""){
     document.getElementById('messages').innerHTML =("Mesasage is empty");
     $('.messages').addClass('status');
+    $('.fullnames').hide();
+    $('.e-mail').hide();
     return false;
 }
     
